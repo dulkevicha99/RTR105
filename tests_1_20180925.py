48b712b04180a5875a3bb9b998ae104e5f769d6c
user <veronikadulkevica@inbox.lv>
2018-09-25 13:51:51 +0300

    20180925_13_45_

---
+++ tests_1_20180925.py
@@ -0,0 +1,46 @@
+
+mans_mainigums = raw_input("Cienijamais lietotajs, ludzu, ievadi simbolu: ")
+
+
+
+# x = mans_mainigums * mans_mainigums
+# can't multiply sequence by non-int of type 'str'
+
+# http://www.cplusplus.com/reference/cstdio/printf/
+print("mans_mainigums = %s"%(mans_mainigums))
+print("respektīvi, tu esi ievadījis skaitli: = %s"%(mans_mainigums))
+print("vēl atmiņā tagad ie arī mainīgais x= %s"%(x))
+
+
+
+
+
+
+
+
+
+
+
+'''
+mans_mainigums = 1. * input("Cienijamais lietotajs, ludzu, ievadi skaitli: ")
+x = mans_mainigums * mans_mainigums
+
+
+# http://www.cplusplus.com/reference/cstdio/printf/
+print("mans_mainigums = %10.3f"%(mans_mainigums))
+print("respektīvi, tu esi ievadījis skaitli: = %10.4f"%(mans_mainigums))
+print("vēl atmiņā tagad ie arī mainīgais x= %15.7f"%(x))
+
+'''
+
+'''
+mans_mainigums = input("Cienijamais lietotajs, ludzu, ievadi skaitli: ")
+x = mans_mainigums * mans_mainigums
+
+
+# http://www.cplusplus.com/reference/cstdio/printf/
+print("mans_mainigums = %d"%(mans_mainigums))
+print("respektīvi, tu esi ievadījis skaitli: = %d"%(mans_mainigums))
+print("vēl atmiņā tagad ie arī mainīgais x= %d"%(x))
+
+'''
