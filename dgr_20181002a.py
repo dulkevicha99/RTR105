f2cfca6160dc39df53fb509dd990d0acec7f2383
user <veronikadulkevica@inbox.lv>
2018-10-02 16:07:14 +0300

    20181002_16_03

---
+++ dgr_20181002a.py
@@ -0,0 +1,429 @@
+Python 2.7.12 (default, Dec  4 2017, 14:50:18) 
+[GCC 5.4.0 20160609] on linux2
+Type "copyright", "credits" or "license()" for more information.
+>>> x= 5
+>>> if x<10
+SyntaxError: invalid syntax
+>>> x=5
+>>> if x < 10:
+	print('Smaller')
+	if x > 20:
+		print('Bigger')
+		print('Finis')
+
+		
+Smaller
+>>> x=5
+>>> if x==5:
+	print('Equals 5')
+	if x > 4:
+		print('Greater than 4')
+		if x < 6 :print('Less than 6')
+		if x < 5:
+			print('Less than or Equals 5')
+			if x!=6:
+				print('Not equal 6')
+
+				
+Equals 5
+Greater than 4
+Less than 6
+>>> if x==5:
+	print('Equals 5')
+if x > 4:
+	print('Greater than 4')
+if x < 6:
+	print('Less than 6')
+if x < 5:
+	print('Less than or Equals 5')
+if x!=6:				print('Not equal 6')
+SyntaxError: invalid syntax
+>>> if x==5:
+	print('Equals 5')
+if x > 4:
+	print('Greater than 4')
+if x < 6:
+	print('Less than 6')
+if x < 5:
+	print('Less than or Equals 5')
+if x!=6:print('Not equal 6')
+SyntaxError: invalid syntax
+>>> if x==5:
+	print('Equals 5')
+if x > 4:
+	print('Greater than 4')
+if x < 6:
+	print('Less than 6')
+if x < 5:
+	print('Less than or Equals 5')
+if x!=6:print('Not equal 6')
+SyntaxError: invalid syntax
+>>> if x==5:
+	print('Equals 5')
+if x > 4:
+	print('Greater than 4')
+if x < 6:
+	print('Less than 6')
+if x < 5:
+	print('Less than or Equals 5')
+if x!=6:print('Not equal 6')
+SyntaxError: invalid syntax
+>>> if x==5:
+	print('Equals 5')
+if x > 4:
+	print('Greater than 4')
+if x < 6:
+	print('Less than 6')
+if x < 5:
+	print('Less than or Equals 5')
+if x!=6:
+	
+SyntaxError: invalid syntax
+>>> if x==5:
+	print('Equals 5')
+if x > 4:
+	print('Greater than 4')
+if x < 6:
+	print('Less than 6')
+if x < 5:
+	print('Less than or Equals 5')
+if x!=6:print('Not equal 6')
+SyntaxError: invalid syntax
+>>> 
+================== RESTART: /home/user/RTR105/if_test_1.py ==================
+
+Traceback (most recent call last):
+  File "/home/user/RTR105/if_test_1.py", line 1, in <module>
+    x == 5
+NameError: name 'x' is not defined
+>>> 
+================== RESTART: /home/user/RTR105/if_test_1.py ==================
+Equals 5
+Greater than 4
+Less than 6
+Not equal 6
+>>> 
+================== RESTART: /home/user/RTR105/if_test_1.py ==================
+Equals 5
+Greater than 4
+Less than 6
+Not equal 6
+>>> 
+================== RESTART: /home/user/RTR105/if_test_1.py ==================
+Equals 5
+Greater than 4
+Greater than or Equals 5
+Less than 6
+Less than or Equals 5
+Not equal 6
+>>> x=5
+>>> 
+>>> x=5
+>>> print('Before 5')
+Before 5
+>>> 
+================== RESTART: /home/user/RTR105/if_test_1.py ==================
+Equals 5
+Greater than 4
+Greater than or Equals 5
+Less than 6
+Less than or Equals 5
+Not equal 6
+Before 5
+ls 5
+ls still 5
+Third 5
+Afrerwards 5
+Before 6
+Afrerwards 6
+>>> 
+================== RESTART: /home/user/RTR105/if_test_1.py ==================
+Equals 5
+Greater than 4
+Greater than or Equals 5
+Less than 6
+Less than or Equals 5
+Not equal 6
+Before 5
+ls 5
+ls still 5
+Third 5
+Afrerwards 5
+Before 6
+Afrerwards 6
+Bigger than 2
+Still bigger
+Done with 2
+0
+1
+2
+3
+4
+Bigger than 2
+('Done with i', 4)
+All Done
+>>> 
+================== RESTART: /home/user/RTR105/if_test_1.py ==================
+Equals 5
+Greater than 4
+Greater than or Equals 5
+Less than 6
+Less than or Equals 5
+Not equal 6
+Before 5
+ls 5
+ls still 5
+Third 5
+Afrerwards 5
+Before 6
+Afrerwards 6
+Bigger than 2
+Still bigger
+Done with 2
+0
+1
+2
+3
+4
+Bigger than 2
+('Done with i', 4)
+All Done
+More than one
+Less than 100
+All done
+>>> 
+================== RESTART: /home/user/RTR105/if_test_1.py ==================
+Equals 5
+Greater than 4
+Greater than or Equals 5
+Less than 6
+Less than or Equals 5
+Not equal 6
+Before 5
+ls 5
+ls still 5
+Third 5
+Afrerwards 5
+Before 6
+Afrerwards 6
+Bigger than 2
+Still bigger
+Done with 2
+0
+1
+2
+3
+4
+Bigger than 2
+('Done with i', 4)
+All Done
+More than one
+Less than 100
+All done
+Bigger
+>>> 
+================== RESTART: /home/user/RTR105/if_test_1.py ==================
+Equals 5
+Greater than 4
+Greater than or Equals 5
+Less than 6
+Less than or Equals 5
+Not equal 6
+Before 5
+ls 5
+ls still 5
+Third 5
+Afrerwards 5
+Before 6
+Afrerwards 6
+Bigger than 2
+Still bigger
+Done with 2
+0
+1
+2
+3
+4
+Bigger than 2
+('Done with i', 4)
+All Done
+More than one
+Less than 100
+All done
+Bigger
+Small
+>>> 
+================== RESTART: /home/user/RTR105/if_test_1.py ==================
+Equals 5
+Greater than 4
+Greater than or Equals 5
+Less than 6
+Less than or Equals 5
+Not equal 6
+Before 5
+ls 5
+ls still 5
+Third 5
+Afrerwards 5
+Before 6
+Afrerwards 6
+Bigger than 2
+Still bigger
+Done with 2
+0
+1
+2
+3
+4
+Bigger than 2
+('Done with i', 4)
+All Done
+More than one
+Less than 100
+All done
+Bigger
+Small
+Small
+>>> 
+================== RESTART: /home/user/RTR105/if_test_1.py ==================
+Equals 5
+Greater than 4
+Greater than or Equals 5
+Less than 6
+Less than or Equals 5
+Not equal 6
+Before 5
+ls 5
+ls still 5
+Third 5
+Afrerwards 5
+Before 6
+Afrerwards 6
+Bigger than 2
+Still bigger
+Done with 2
+0
+1
+2
+3
+4
+Bigger than 2
+('Done with i', 4)
+All Done
+More than one
+Less than 100
+All done
+Bigger
+Small
+Small
+LARGE
+All done
+>>> 
+================== RESTART: /home/user/RTR105/if_test_1.py ==================
+Equals 5
+Greater than 4
+Greater than or Equals 5
+Less than 6
+Less than or Equals 5
+Not equal 6
+Before 5
+ls 5
+ls still 5
+Third 5
+Afrerwards 5
+Before 6
+Afrerwards 6
+Bigger than 2
+Still bigger
+Done with 2
+0
+1
+2
+3
+4
+Bigger than 2
+('Done with i', 4)
+All Done
+More than one
+Less than 100
+All done
+Bigger
+Small
+Small
+LARGE
+All done
+All done
+>>> 
+================== RESTART: /home/user/RTR105/if_test_1.py ==================
+Equals 5
+Greater than 4
+Greater than or Equals 5
+Less than 6
+Less than or Equals 5
+Not equal 6
+Before 5
+ls 5
+ls still 5
+Third 5
+Afrerwards 5
+Before 6
+Afrerwards 6
+Bigger than 2
+Still bigger
+Done with 2
+0
+1
+2
+3
+4
+Bigger than 2
+('Done with i', 4)
+All Done
+More than one
+Less than 100
+All done
+Bigger
+Small
+Small
+LARGE
+All done
+All done
+Two or more
+>>> 
+================== RESTART: /home/user/RTR105/if_test_1.py ==================
+Equals 5
+Greater than 4
+Greater than or Equals 5
+Less than 6
+Less than or Equals 5
+Not equal 6
+Before 5
+ls 5
+ls still 5
+Third 5
+Afrerwards 5
+Before 6
+Afrerwards 6
+Bigger than 2
+Still bigger
+Done with 2
+0
+1
+2
+3
+4
+Bigger than 2
+('Done with i', 4)
+All Done
+More than one
+Less than 100
+All done
+Bigger
+Small
+Small
+LARGE
+All done
+All done
+Two or more
+Below 10
+>>> 
---
+++ if_test_1.py
@@ -0,0 +1,137 @@
+x = 5
+
+
+if x==5:
+    print('Equals 5')
+if x > 4:
+    print('Greater than 4')
+    if x >=5:
+        print('Greater than or Equals 5')
+if x < 6:
+    print('Less than 6')
+if x <= 5:
+    print('Less than or Equals 5')
+if x!=6:
+    print('Not equal 6')
+x=5
+print('Before 5')
+if x==5:
+    print('ls 5')
+    print('ls still 5')
+    print ('Third 5')
+print('Afrerwards 5')
+print('Before 6')
+if x==6:
+    print('ls 6')
+    print('ls still 6')
+    print ('Third 6')
+print('Afrerwards 6')
+
+
+x=5
+if x > 2:
+    print('Bigger than 2')
+    print('Still bigger')
+print('Done with 2')
+
+for i in range(5):
+   print(i)
+if i > 2:
+    print('Bigger than 2')
+    print('Done with i',i)
+    print('All Done')
+
+
+x=42
+if x > 1:
+    print('More than one')
+    if x < 100:
+        print('Less than 100')
+        print('All done')
+
+
+x=4
+if x > 2:
+    print('Bigger')
+else:
+    print('Smaller')
+    print('All done')
+
+         
+        
+        
+x=0
+if x < 2:
+    print('Small')
+elif x < 10:
+    print('Medium')
+else:
+    print('LARGE')
+    print('All done')
+
+
+x=5
+if x < 10:
+    print('Small')
+elif x < 10:
+    print('Medium')
+else:
+    print('LARGE')
+    print('All done')
+    
+
+
+
+
+x=20
+if x < 2:
+    print('small')
+elif x < 10:
+    print ('medium')
+else:
+    print('LARGE')
+    print('All done')
+
+
+x=5
+if x < 2:
+    print('small')
+elif x < 10:
+    print('All done')
+
+
+
+if x < 2:
+    print('Below 2')
+elif x >=2:
+    print('Two or more')
+else:
+    print('Something else')
+
+
+if x < 2:
+     print('Below 2')
+elif x < 0:
+     print('Below 20')
+elif x < 10:
+    print('Below 10')
+else:
+    print('Something else')
+
+
+
+$cat notry.py
+astr = 'Hello Bob'
+istr = int(astr)
+print('First',istr)
+astr = '123'
+
+istr = int(astr)
+print('Second',istr)
+
+
+    
+    
+     
+    
+     
