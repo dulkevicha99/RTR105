921d288f31bc4926ba1e79f3778e91383d4fb807
user <veronikadulkevica@inbox.lv>
2018-10-09 14:05:09 +0300

    201811009_14_05

---
+++ dgr_20181009.py
@@ -0,0 +1,35 @@
+Python 2.7.12 (default, Dec  4 2017, 14:50:18) 
+[GCC 5.4.0 20160609] on linux2
+Type "copyright", "credits" or "license()" for more information.
+>>> x=5
+>>> if x==5:
+	print('Equals 5')
+    if x>4:
+	    
+  File "<pyshell#3>", line 3
+    if x>4:
+          ^
+IndentationError: unindent does not match any outer indentation level
+>>> 
+==================== RESTART: /home/user/RTR105/test_1.py ====================
+Equals 5
+Greater than 4
+Greater than or Equals 5
+Less than 6
+Less than or Equals 5
+Not equal 6
+>>> 
+==================== RESTART: /home/user/RTR105/test_1.py ====================
+Equals 5
+Greater than 4
+Greater than or Equals 5
+Less than 6
+Less than or Equals 5
+Not equal 6
+Before 5
+Is 5
+Is Still 5
+Third 5
+Afterwards 5
+Before 6
+>>> 
---
+++ test_1.py
@@ -0,0 +1,26 @@
+x=5
+if x==5:
+    print('Equals 5')
+    if x>4:
+        print('Greater than 4')
+        if x>=5:
+            print('Greater than or Equals 5')
+            if x<6:print('Less than 6')
+            if x<=5:
+                print('Less than or Equals 5')
+                if x!=6: print('Not equal 6')
+                
+x=5
+print('Before 5')
+if x==5:
+    print('Is 5')
+    print('Is Still 5')
+    print('Third 5')
+    print('Afterwards 5')
+    print('Before 6')
+    if x==6:
+        print('Is 6')
+        print('Is Still 6')
+        print('Third')
+        print('Afterwards ')
+        
