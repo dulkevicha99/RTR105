786ae220334683fda1c1a4b1f05ef58fac82b3f0
user <veronikadulkevica@inbox.lv>
2018-11-27 14:48:19 +0200

    20181127

---
+++ monteCarlo.a.py
@@ -0,0 +1,37 @@
+Python 2.7.12 (default, Dec  4 2017, 14:50:18) 
+[GCC 5.4.0 20160609] on linux2
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+================== RESTART: /home/user/RTR105/MonteCarlo.py ==================
+
+================== RESTART: /home/user/RTR105/MonteCarlo.py ==================
+
+Traceback (most recent call last):
+  File "/home/user/RTR105/MonteCarlo.py", line 43, in <module>
+    N1=N1+1
+NameError: name 'N1' is not defined
+>>> 
+================== RESTART: /home/user/RTR105/MonteCarlo.py ==================
+
+================== RESTART: /home/user/RTR105/MonteCarlo.py ==================
+
+Traceback (most recent call last):
+  File "/home/user/RTR105/MonteCarlo.py", line 50, in <module>
+    print(S_nezinamais)
+NameError: name 'S_nezinamais' is not defined
+>>> 
+================== RESTART: /home/user/RTR105/MonteCarlo.py ==================
+12.275
+>>> 
+================== RESTART: /home/user/RTR105/MonteCarlo.py ==================
+12.05
+>>> 
+================== RESTART: /home/user/RTR105/MonteCarlo.py ==================
+12.3
+>>> 
+================== RESTART: /home/user/RTR105/MonteCarlo.py ==================
+12.435
+>>> 
+================== RESTART: /home/user/RTR105/MonteCarlo.py ==================
+12.66
+>>> 
