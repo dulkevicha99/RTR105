3291991f81fcfad5eb490d129c33f0b5e2587f22
user <veronikadulkevica@inbox.lv>
2018-12-11 15:01:28 +0200

    20181211.15.4

--- labaratorka04.py
+++ labaratorka04.py
@@ -5,19 +5,19 @@ from numpy import random,sqrt, sin
 #from math import sqrt
 #from math import sin
 
-N = 3000
+N = 7000
 N1=0
 
 x= random.uniform(0,5,N)
 y= random.uniform(0,5,N)
 
-funct=sin(x)*sin(x)
+funct=(sin(x)*sin(x))/2
 
 from matplotlib import pyplot as plt
 plt.grid()
 plt.xlabel('x')
 plt.ylabel('f(x)')
-plt.title('Funkcija $sin(x)*sin(x)$')
+plt.title('Funkcija $(sin(x)*sin(x))/2$')
 plt.plot(x,funct,'o', color = "#990000")
 for i in range(N):
     #plt.plot(x[i],y[i],"ko")
