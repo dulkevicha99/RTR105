d31e86e9ad8227faab97f8646010d3b5301868d4
user <veronikadulkevica@inbox.lv>
2018-12-11 14:48:46 +0200

    20181211.14

--- labar03.py
+++ labar03.py
@@ -1,5 +1,6 @@
 #Aptuveni novērojot funkcijas saknes vertību lietojot "pēli" iegūstam x=
 
+           
 from math import sin,fabs
 from time import sleep
 
@@ -11,6 +12,17 @@ b= 1
 
 funa =f(a)
 funb =f(b)
+
+from numpy import*
+from matplotlib import pyplot as plt
+x= linspace(0,4,70)
+y= (sin(x) * sin(x))/2
+plt.grid()
+plt.xlabel('x') 
+plt.ylabel('f(x)') 
+plt.title('Funkcijas $(sin(s)*sin(x))/2$')
+plt.plot(x,y,color = '#FF0000',linewidth =5.0)
+plt.show() 
 
 if (funa*funb >0.0):
     print('Dotāja intervala[%s,%s] sakņu nav'%(a,b))
