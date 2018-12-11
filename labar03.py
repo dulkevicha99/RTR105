ede8cc9df499087a1577fb41d831072144b408d5
dulkevicha99 <42965243+dulkevicha99@users.noreply.github.com>
2018-12-11 13:05:19 +0200

    Update labar03.py

--- labar03.py
+++ labar03.py
@@ -5,24 +5,23 @@ from time import sleep
 
 def f(x):
     return sin(x/2)*sin(x/2)
-k=0.5
-a=1.1
-b=3.2
+k=0
+a= -1
+b=1
 
 funa =f(a)
 funb =f(b)
 
 if (funa*funb >0.0):
-    print('Dotāja intervala[%s,%s] sakņu nav')%(a,b)
+    print('Dotāja intervala[%s,%s] sakņu nav'%(a,b))
     sleep(1);exit()
 else:
-    print('Dotāja intervala saknes(s) ir!')
+    print('Dotāja intervala sakne(s) ir!')
 deltax= 0.0001
 
-k=0
-while(fabs(b-a)> delatax):
+while(fabs(b-a)> deltax):
     k=k+1
-    x=(a+b)/2;funx=f(x)
+    x=(a+b)/2.;funx=f(x)
     if(funa*funx<0.):
         b=x
     else:
